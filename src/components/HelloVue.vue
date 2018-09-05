f97c8b607111f00484125d11a14f06d303ea4064
liguodong <842079293@qq.com>
2018-09-05 15:55:22 +0800

    This is a todolist based on Vue

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <p>{{ msg }}</p>
+  <button v-on:click="handleAdd">You have click me {{ count }} times.</button>
+  <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloVue',
+  data () {
+    return {
+      msg: 'hahaha',
+      title: 'this is a vue app',
+      count: 0
+    }
+  },
+  methods: {
+    handleAdd () {
+      this.count++
+      this.title = 'you has clicked the button'
+    }
+  }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <input id="inputA" type="text" class="inputT" placeholder="Input things that you want to do" v-model="newItem">
+    <button v-on:click="addNewItem(newItem)">ADD</button>
+    <h2 class="h2center">The things you will to do:</h2>
+    <ul class="forSoncenter">
+      <li class="forRe" v-for="(item, index) in items" v-bind:key="index">
+        {{ item }}
+        <span class="remove" v-on:click="handleRemove(index)">X</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      items: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    addNewItem (item) {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+    handleRemove (index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+<style>
+.inputT {
+  width: 200px;
+}
+.forRe {
+  width: 295px;
+  height: 30px;
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: bold;
+  border: 1px solid black;
+  text-align: left;
+  list-style-type: none;
+  background-color: aquamarine;
+  margin-bottom: 5px;
+  padding-left: 5px;
+}
+.forSoncenter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.h2center {
+  margin: auto auto;
+  padding-top: 20px;
+}
+.remove {
+  font-size: 28px;
+  float:right;
+  color: red;
+  cursor: pointer;
+}
+</style>
